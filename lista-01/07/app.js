e5db7d0e4c8f003e2a8232ab5df9d03940d9e4be
Gláucia Andare <glauciaandare@gmail.com>
2025-05-23 21:33:53 -0300

    Entrega exercicio 07

---
+++ lista-01/07/app.js
@@ -0,0 +1,23 @@
+
+const readline = require('readline');
+
+const entrada = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+entrada.question("Digite o número de maçãs compradas: ", function(qtdStr) {
+    const quantidade = parseInt(qtdStr);
+
+    if (isNaN(quantidade) || quantidade <= 0) {
+        console.log("Quantidade inválida.");
+    } else {
+        const preco = quantidade < 12 ? 0.30 : 0.25;
+        const total = quantidade * preco;
+
+        console.log(`Preço unitário: R$ ${preco.toFixed(2)}`);
+        console.log(`Valor total da compra: R$ ${total.toFixed(2)}`);
+    }
+
+    entrada.close();
+});
